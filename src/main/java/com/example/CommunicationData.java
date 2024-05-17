590a9ba16811e5bb42f5c7ab454e3c16e982e90e
vhdez <vhernandez@scienceleadership.org>
2024-05-17 09:26:13 -0400

    CommunicationData serialVersionUID is Long.

--- src/main/java/com/example/CommunicationData.java
+++ src/main/java/com/example/CommunicationData.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.net.InetAddress;
 
 public class CommunicationData implements Serializable {
-    private static final long serialVersionUID = 1234567890;
+    private static final long serialVersionUID = 1234567890L;
 
     String from;
     String to;
